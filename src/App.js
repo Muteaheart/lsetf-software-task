e7b1de04bd42bb3e1df7c89ca414f42228ea3214
Muteaheart <mustaphamutiat4434@gmail.com>
2022-03-21 13:08:24 +0100

    lsetf-software-task

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components"
+import Card from "./Components/Card/Card"
+import Footer from "./Components/Footer/Footer"
+import Header from "./Components/Header/Header"
+
+function App() {
+  return (
+    <>
+<Header />
+<Card />
+<Footer />
+
+    </>
+  )
+}
+
+export default App
